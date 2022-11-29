7c13b93f05e12ac89e45de08d6765e95d1e03268
Developer <noob@developer.com>
2022-11-29 17:06:53 +0000

    Modifying auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,7 +33,7 @@ const forgotPassword = catchAsync(async (req, res) => {
 #
 const resetPassword = catchAsync(async (req, res) => {
   await authService.resetPassword(req.query.token, req.body.password);
-  res.status(httpStatus.NO_CONTENT).send();
+#  res.status(httpStatus.NO_CONTENT).send();
 #});
 
 const sendVerificationEmail = catchAsync(async (req, res) => {
