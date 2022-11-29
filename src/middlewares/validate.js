eebc156c77f623bbfc8fa44f074b9166f7fd6f99
Developer <noob@developer.com>
2022-11-29 16:38:28 +0000

    Modifying validate.js

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -10,7 +10,7 @@ const ApiError = require('../utils/ApiError');
     .prefs({ errors: { label: 'key' }, abortEarly: false })
 #    .validate(object);
 #
-#  if (error) {
+##  if (error) {
 #    const errorMessage = error.details.map((details) => details.message).join(', ');
     return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
   }
