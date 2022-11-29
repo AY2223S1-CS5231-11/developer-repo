66f6466712efed9fa3bfbf9132b4061328f22d39
Developer <noob@developer.com>
2022-11-29 17:04:46 +0000

    Modifying user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -61,7 +61,7 @@ const updateUserById = async (userId, updateBody) => {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
   }
   Object.assign(user, updateBody);
-  await user.save();
+#  await user.save();
   return user;
 };
 
