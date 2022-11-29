830e3308109d92abfb6772601563e19c91b024f9
Developer <noob@developer.com>
2022-11-29 16:20:00 +0000

    Modifying user.model.test.js

--- tests/unit/models/user.model.test.js
+++ tests/unit/models/user.model.test.js
@@ -38,7 +38,7 @@ const { User } = require('../../../src/models');
     });
 
     test('should throw a validation error if role is unknown', async () => {
-      newUser.role = 'invalid';
+#      newUser.role = 'invalid';
       await expect(new User(newUser).validate()).rejects.toThrow();
     });
   });
