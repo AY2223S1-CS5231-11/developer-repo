c3be669bfe280aa77005966197777f22c61ce52d
Developer <noob@developer.com>
2022-11-29 16:44:42 +0000

    Modifying user.test.js

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -242,7 +242,7 @@ describe('User routes', () => {
         limit: 10,
         totalPages: 1,
         totalResults: 3,
-      });
+#      });
       expect(res.body.results).toHaveLength(3);
       expect(res.body.results[0].id).toBe(userOne._id.toHexString());
       expect(res.body.results[1].id).toBe(userTwo._id.toHexString());
