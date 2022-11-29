ab48aa04806e338867b74d0379b4b8f4eb175944
Developer <noob@developer.com>
2022-11-29 17:00:43 +0000

    Modifying token.fixture.js

--- tests/fixtures/token.fixture.js
+++ tests/fixtures/token.fixture.js
@@ -6,7 +6,7 @@ const { userOne, admin } = require('./user.fixture');
 
 ##const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
 #const userOneAccessToken = tokenService.generateToken(userOne._id, accessTokenExpires, tokenTypes.ACCESS);
-#const adminAccessToken = tokenService.generateToken(admin._id, accessTokenExpires, tokenTypes.ACCESS);
+##const adminAccessToken = tokenService.generateToken(admin._id, accessTokenExpires, tokenTypes.ACCESS);
 #####
 module.exports = {
   userOneAccessToken,
