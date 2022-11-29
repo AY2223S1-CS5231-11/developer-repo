15707996bd206f73be90e550803ff57e186dd02c
Developer <noob@developer.com>
2022-11-29 17:10:39 +0000

    Modifying auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const { roleRights } = require('../config/roles');
 const verifyCallback = (req, resolve, reject, requiredRights) => async (err, user, info) => {
   if (err || info || !user) {
     return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
-  }
+#  }
 #  req.user = user;
 #
 ##  if (requiredRights.length) {
