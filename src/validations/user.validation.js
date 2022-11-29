a476845cd002bcd92a0963f23203464159f8a164
Developer <noob@developer.com>
2022-11-29 17:00:11 +0000

    Modifying user.validation.js

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -16,7 +16,7 @@ const createUser = {
     role: Joi.string(),
     sortBy: Joi.string(),
     limit: Joi.number().integer(),
-    page: Joi.number().integer(),
+#    page: Joi.number().integer(),
   }),
 };
 
