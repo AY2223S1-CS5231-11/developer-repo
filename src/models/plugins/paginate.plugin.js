73af510233414291c3ee8fadce16f13c844034d8
Developer <noob@developer.com>
2022-11-29 17:19:00 +0000

    Modifying paginate.plugin.js

--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -33,7 +33,7 @@ const paginate = (schema) => {
     }
 #
     const limit = options.limit && parseInt(options.limit, 10) > 0 ? parseInt(options.limit, 10) : 10;
-    const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
+#    const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
     const skip = (page - 1) * limit;
 #
     const countPromise = this.countDocuments(filter).exec();
