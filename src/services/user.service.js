d92ab2633fe80a36ab61aba0eac1d98891005621
Developer <noob@developer.com>
2022-11-29 17:13:40 +0000

    Modifying user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -20,7 +20,7 @@ const createUser = async (userBody) => {
 # * @param {Object} options - Query options
  * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
  * @param {number} [options.limit] - Maximum number of results per page (default = 10)
-# * @param {number} [options.page] - Current page (default = 1)
+## * @param {number} [options.page] - Current page (default = 1)
  * @returns {Promise<QueryResult>}
  */
 const queryUsers = async (filter, options) => {
