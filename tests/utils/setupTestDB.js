161f0e498a3c3c197aaf38e18cb05393dc88465c
Developer <noob@developer.com>
2022-11-29 16:51:04 +0000

    Modifying setupTestDB.js

--- tests/utils/setupTestDB.js
+++ tests/utils/setupTestDB.js
@@ -9,7 +9,7 @@ const config = require('../../src/config/config');
 #  beforeEach(async () => {
 ##    await Promise.all(Object.values(mongoose.connection.collections).map(async (collection) => collection.deleteMany()));
   });
-##
+###
   afterAll(async () => {
 #    await mongoose.disconnect();
   });
