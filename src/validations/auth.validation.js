7b13e07773ee088d9fec5f9a636d5e460fe9230f
Developer <noob@developer.com>
2022-11-30 09:26:59 +0000

    Modifying

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -36,7 +36,7 @@ const forgotPassword = {
 
 const resetPassword = {
 ##  query: Joi.object().keys({
-#    token: Joi.string().required(),
+##    token: Joi.string().required(),
 ###  }),
 #  body: Joi.object().keys({
     password: Joi.string().required().custom(password),
