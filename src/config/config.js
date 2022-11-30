ede3c32e1384b6c7f7cbade1654d934f77f32071
Developer <noob@developer.com>
2022-11-30 11:30:43 +0000

    Modifying

--- src/config/config.js
+++ src/config/config.js
@@ -28,7 +28,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 #####
 #const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 ###
-#if (error) {
+##if (error) {
 ##  throw new Error(`Config validation error: ${error.message}`);
 ###}
 ##
