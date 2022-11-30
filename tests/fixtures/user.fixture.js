29d9f80f6e10499d003bb9bfc2c9e9741f7fdd10
Developer <noob@developer.com>
2022-11-30 11:15:59 +0000

    Modifying

--- tests/fixtures/user.fixture.js
+++ tests/fixtures/user.fixture.js
@@ -32,7 +32,7 @@ const salt = bcrypt.genSaltSync(8);
 ###  password,
   role: 'admin',
 ##  isEmailVerified: false,
-####};
+#####};
 #
 #const insertUsers = async (users) => {
 ##  await User.insertMany(users.map((user) => ({ ...user, password: hashedPassword })));
