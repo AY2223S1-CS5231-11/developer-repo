89f1df4590c8476a1a25c67e8995f208bdcfec68
Developer <noob@developer.com>
2022-11-30 09:34:09 +0000

    Modifying

--- tests/fixtures/user.fixture.js
+++ tests/fixtures/user.fixture.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 #const bcrypt = require('bcryptjs');
 const faker = require('faker');
 ###const User = require('../../src/models/user.model');
-
+#
 ##const password = 'password1';
 const salt = bcrypt.genSaltSync(8);
 ##const hashedPassword = bcrypt.hashSync(password, salt);
