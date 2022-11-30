688a38eb8066f9754cee4802d56b12997b35a8f7
Developer <noob@developer.com>
2022-11-30 09:16:25 +0000

    Modifying

--- src/config/morgan.js
+++ src/config/morgan.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 ####const getIpFormat = () => (config.env === 'production' ? ':remote-addr - ' : '');
 ###const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`;
 #const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`;
-
+#
 ####const successHandler = morgan(successResponseFormat, {
 ####  skip: (req, res) => res.statusCode >= 400,
 ##  stream: { write: (message) => logger.info(message.trim()) },
