2993344f4bbc6c685756eb955f84c18a50811f8a
Developer <noob@developer.com>
2022-11-30 09:27:07 +0000

    Modifying

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const { password } = require('./custom.validation');
-##
+###
 const register = {
   body: Joi.object().keys({
 #    email: Joi.string().required().email(),
