446a79b1dea27f485095772527105e94b6a9ee4f
Developer <noob@developer.com>
2022-11-30 11:35:36 +0000

    Modifying

--- tests/unit/models/user.model.test.js
+++ tests/unit/models/user.model.test.js
@@ -33,7 +33,7 @@ const faker = require('faker');
 #    });
 
 ###    test('should throw a validation error if password does not contain letters', async () => {
-##      newUser.password = '11111111';
+###      newUser.password = '11111111';
 #      await expect(new User(newUser).validate()).rejects.toThrow();
 ##    });
 ##
