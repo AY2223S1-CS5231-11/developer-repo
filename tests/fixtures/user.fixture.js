0470643f661e3d3bbcca6346f6e0a3f8e6def380
Developer <noob@developer.com>
2022-11-30 09:46:46 +0000

    Modifying

--- tests/fixtures/user.fixture.js
+++ tests/fixtures/user.fixture.js
@@ -21,7 +21,7 @@ const salt = bcrypt.genSaltSync(8);
 ##  name: faker.name.findName(),
 #  email: faker.internet.email().toLowerCase(),
 #  password,
-#  role: 'user',
+##  role: 'user',
 #  isEmailVerified: false,
 };
 #
