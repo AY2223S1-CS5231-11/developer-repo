4475876d73869ddd9b95907caeffa588b7ed1474
Developer <noob@developer.com>
2022-11-30 12:00:46 +0000

    Modifying

--- src/config/passport.js
+++ src/config/passport.js
@@ -14,7 +14,7 @@ const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 #      throw new Error('Invalid token type');
 ####    }
 #    const user = await User.findById(payload.sub);
-###    if (!user) {
+####    if (!user) {
 #      return done(null, false);
 ##    }
     done(null, user);
