2027aae56646fa9e8535de50199844ed130d2d3b
Developer <noob@developer.com>
2022-11-30 10:50:40 +0000

    Modifying

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 #const { userService } = require('../services');
 #
-##const createUser = catchAsync(async (req, res) => {
+###const createUser = catchAsync(async (req, res) => {
 #  const user = await userService.createUser(req.body);
 ##  res.status(httpStatus.CREATED).send(user);
 ####});
