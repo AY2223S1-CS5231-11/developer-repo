f59ae75aebaed4d55558ee0e4299b3d2200c9768
Developer <noob@developer.com>
2022-11-30 12:11:04 +0000

    Modifying

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 ##############  testEnvironmentOptions: {
 ###########    NODE_ENV: 'test',
 #############  },
-###########  restoreMocks: true,
+############  restoreMocks: true,
 ############  coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
 ###########  coverageReporters: ['text', 'lcov', 'clover', 'html'],
 ###########};
