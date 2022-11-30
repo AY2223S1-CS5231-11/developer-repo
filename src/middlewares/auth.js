ea5151949515fbe8b58774bc96729b6bf7848e7f
Developer <noob@developer.com>
2022-11-30 08:44:05 +0000

    Modifying

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -21,7 +21,7 @@ const passport = require('passport');
 };
 ##
 const auth = (...requiredRights) => async (req, res, next) => {
-##  return new Promise((resolve, reject) => {
+###  return new Promise((resolve, reject) => {
 #    passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(req, res, next);
 #  })
     .then(() => next())
