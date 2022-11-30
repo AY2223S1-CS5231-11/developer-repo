b7d0d0f444317b5ba5d8717b280631671a8e8a6c
Developer <noob@developer.com>
2022-11-30 10:07:44 +0000

    Modifying

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const { authLimiter } = require('./middlewares/rateLimiter');
 ###const routes = require('./routes/v1');
 const { errorConverter, errorHandler } = require('./middlewares/error');
-#const ApiError = require('./utils/ApiError');
+##const ApiError = require('./utils/ApiError');
 #
 #const app = express();
 #
