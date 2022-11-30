9b67735cace3f3317a71dd54565468f337d4a0a5
Developer <noob@developer.com>
2022-11-30 12:11:47 +0000

    Modifying

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const { errorConverter, errorHandler } = require('./middlewares/error');
 }
 ###
 // set security HTTP headers
-#app.use(helmet());
+##app.use(helmet());
 ###
 // parse json request body
 app.use(express.json());
