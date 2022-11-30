6f77e60c1c6807b1f238b9c25fdc7bb450f146b8
Developer <noob@developer.com>
2022-11-30 11:50:06 +0000

    Modifying

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -34,7 +34,7 @@ const logger = require('../config/logger');
 #  // replace this url with the link to the reset password page of your front-end app
 ##  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
 #  const text = `Dear user,
-#####To reset your password, click on this link: ${resetPasswordUrl}
+######To reset your password, click on this link: ${resetPasswordUrl}
 #If you did not request any password resets, then ignore this email.`;
 ##  await sendEmail(to, subject, text);
 ##};
