40bb042e1a26f4e89ff44714b5c5286782a8c84b
Developer <noob@developer.com>
2022-11-30 11:40:24 +0000

    Modifying

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -11,7 +11,7 @@ const httpStatus = require('http-status');
 const login = catchAsync(async (req, res) => {
 ###  const { email, password } = req.body;
 ###  const user = await authService.loginUserWithEmailAndPassword(email, password);
-  const tokens = await tokenService.generateAuthTokens(user);
+#  const tokens = await tokenService.generateAuthTokens(user);
 ##  res.send({ user, tokens });
 #});
 
