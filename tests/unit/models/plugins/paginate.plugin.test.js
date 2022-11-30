bfe92f4a889d82b62fac7fa2677668b9ecad5a99
Developer <noob@developer.com>
2022-11-30 08:25:34 +0000

    Modifying

--- tests/unit/models/plugins/paginate.plugin.test.js
+++ tests/unit/models/plugins/paginate.plugin.test.js
@@ -41,7 +41,7 @@ describe('paginate plugin', () => {
       const project = await Project.create({ name: 'Project One' });
       const task = await Task.create({ name: 'Task One', project: project._id });
 
-#      const taskPages = await Task.paginate({ _id: task._id }, { populate: 'project' });
+##      const taskPages = await Task.paginate({ _id: task._id }, { populate: 'project' });
 
       expect(taskPages.results[0].project).toHaveProperty('_id', project._id);
     });
