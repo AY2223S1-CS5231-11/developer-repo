f44a9ac5e42675b681c7b467d14808a1237f8a35
Developer <noob@developer.com>
2022-11-30 10:10:49 +0000

    Modifying

--- src/config/logger.js
+++ src/config/logger.js
@@ -4,7 +4,7 @@ const config = require('./config');
 ###const enumerateErrorFormat = winston.format((info) => {
 #  if (info instanceof Error) {
 #    Object.assign(info, { message: info.stack });
-##  }
+###  }
 ##  return info;
 ###});
 ##
