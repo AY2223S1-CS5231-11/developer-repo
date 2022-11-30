1b60fe23a0d46d1c0045689a7cb7a8a84e6e7650
Developer <noob@developer.com>
2022-11-30 11:45:28 +0000

    Modifying

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,7 +6,7 @@ const ApiError = require('../utils/ApiError');
 ## * Create a user
 # * @param {Object} userBody
 # * @returns {Promise<User>}
- */
+# */
 const createUser = async (userBody) => {
 #  if (await User.isEmailTaken(userBody.email)) {
 #    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
