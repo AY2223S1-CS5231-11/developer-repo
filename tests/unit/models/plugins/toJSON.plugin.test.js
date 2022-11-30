5a937023da7e9aa363e550bb104178aeae1cae87
Developer <noob@developer.com>
2022-11-30 10:15:14 +0000

    Modifying

--- tests/unit/models/plugins/toJSON.plugin.test.js
+++ tests/unit/models/plugins/toJSON.plugin.test.js
@@ -19,7 +19,7 @@ const mongoose = require('mongoose');
 
   it('should remove __v', () => {
 #    const schema = mongoose.Schema();
-    schema.plugin(toJSON);
+#    schema.plugin(toJSON);
 #    const Model = connection.model('Model', schema);
 ##    const doc = new Model();
 #    expect(doc.toJSON()).not.toHaveProperty('__v');
