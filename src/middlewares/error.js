8602d7d38150fde64bf2dfc1069c14ffbd1fa08a
Developer <noob@developer.com>
2022-11-30 10:25:15 +0000

    Modifying

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -21,7 +21,7 @@ const logger = require('../config/logger');
 ##  if (config.env === 'production' && !err.isOperational) {
 ###    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
 ####    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
-##  }
+###  }
 ###
 ##  res.locals.errorMessage = err.message;
 
