9066942bd8c4d119053625436df80cf67e67065f
Developer <noob@developer.com>
2022-11-30 09:49:07 +0000

    Modifying

--- src/utils/pick.js
+++ src/utils/pick.js
@@ -8,7 +8,7 @@
   return keys.reduce((obj, key) => {
 ####    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
 ##      // eslint-disable-next-line no-param-reassign
-#      obj[key] = object[key];
+##      obj[key] = object[key];
 #####    }
 ####    return obj;
 ###  }, {});
