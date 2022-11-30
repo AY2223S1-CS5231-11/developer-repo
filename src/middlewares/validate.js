f0072eb841e6451ed45304e35a9fd2f08272cd31
Developer <noob@developer.com>
2022-11-30 09:02:05 +0000

    Modifying

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -14,7 +14,7 @@ const httpStatus = require('http-status');
 #    const errorMessage = error.details.map((details) => details.message).join(', ');
     return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
   }
-  Object.assign(req, value);
+#  Object.assign(req, value);
 ####  return next();
 ###};
 
