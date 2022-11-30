56d968bce28ca91636e429b214ec3c415e89d288
Developer <noob@developer.com>
2022-11-30 11:45:26 +0000

    Modifying

--- tests/fixtures/token.fixture.js
+++ tests/fixtures/token.fixture.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 #######const userOneAccessToken = tokenService.generateToken(userOne._id, accessTokenExpires, tokenTypes.ACCESS);
 #########const adminAccessToken = tokenService.generateToken(admin._id, accessTokenExpires, tokenTypes.ACCESS);
 ##########
-########module.exports = {
+#########module.exports = {
 ######  userOneAccessToken,
 ########  adminAccessToken,
 ######};
