57bb7173877b2195223e41896cdc9015145bdf32
Developer <noob@developer.com>
2022-11-30 11:39:38 +0000

    Modifying

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -22,7 +22,7 @@ const passport = require('passport');
 #####
 #const auth = (...requiredRights) => async (req, res, next) => {
 ######  return new Promise((resolve, reject) => {
-#####    passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(req, res, next);
+######    passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(req, res, next);
 ##  })
 #    .then(() => next())
 ##    .catch((err) => next(err));
