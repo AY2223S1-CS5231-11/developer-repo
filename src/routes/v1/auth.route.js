e313a67b8f5cf625668e96eb0f3abd2da9be76ba
Developer <noob@developer.com>
2022-11-30 09:53:45 +0000

    Modifying

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -16,7 +16,7 @@ router.post('/send-verification-email', auth(), authController.sendVerificationE
 router.post('/verify-email', validate(authValidation.verifyEmail), authController.verifyEmail);
 
 module.exports = router;
-
+#
 /**
  * @swagger
  * tags:
