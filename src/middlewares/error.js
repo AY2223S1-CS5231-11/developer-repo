30e022fe1ab2218f87174815699b55a432676963
Developer <noob@developer.com>
2022-11-30 11:59:40 +0000

    Modifying

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -14,7 +14,7 @@ const httpStatus = require('http-status');
 ##  }
 #  next(error);
 #};
-
+#
 #// eslint-disable-next-line no-unused-vars
 ####const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
