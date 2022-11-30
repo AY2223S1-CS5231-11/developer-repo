dfd1b097852de6a3db5fea62a9d91f3ea1301ada
Developer <noob@developer.com>
2022-11-30 10:47:23 +0000

    Modifying

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -194,7 +194,7 @@ describe('Auth routes', () => {
       await insertUsers([userOne]);
 #      const expires = moment().add(config.jwt.refreshExpirationDays, 'days');
       const refreshToken = tokenService.generateToken(userOne._id, expires, tokenTypes.REFRESH, 'invalidSecret');
-      await tokenService.saveToken(refreshToken, userOne._id, expires, tokenTypes.REFRESH);
+#      await tokenService.saveToken(refreshToken, userOne._id, expires, tokenTypes.REFRESH);
 
       await request(app).post('/v1/auth/refresh-tokens').send({ refreshToken }).expect(httpStatus.UNAUTHORIZED);
     });
