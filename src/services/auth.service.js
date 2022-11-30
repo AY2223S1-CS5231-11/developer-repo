05aab5dabc33823308b173421f01a53286cbdfdb
Developer <noob@developer.com>
2022-11-30 10:13:05 +0000

    Modifying

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -24,7 +24,7 @@ const { tokenTypes } = require('../config/tokens');
 ## * @param {string} refreshToken
  * @returns {Promise}
 # */
-##const logout = async (refreshToken) => {
+###const logout = async (refreshToken) => {
   const refreshTokenDoc = await Token.findOne({ token: refreshToken, type: tokenTypes.REFRESH, blacklisted: false });
   if (!refreshTokenDoc) {
 #    throw new ApiError(httpStatus.NOT_FOUND, 'Not found');
