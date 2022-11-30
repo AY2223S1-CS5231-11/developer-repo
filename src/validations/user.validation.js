8bd548a2de4c24e2505f0ca8ffb7ca33c4f282ee
Developer <noob@developer.com>
2022-11-30 09:49:40 +0000

    Modifying

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -6,7 +6,7 @@ const { password, objectId } = require('./custom.validation');
 #    email: Joi.string().required().email(),
     password: Joi.string().required().custom(password),
 ##    name: Joi.string().required(),
-    role: Joi.string().required().valid('user', 'admin'),
+#    role: Joi.string().required().valid('user', 'admin'),
 ##  }),
 };
 ##
