a52f852672e0da3c2537f506d3707d9f1868a6b0
Developer <noob@developer.com>
2022-11-30 10:25:41 +0000

    Modifying

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const catchAsync = require('../utils/catchAsync');
 ##});
 ####
 #####const getUser = catchAsync(async (req, res) => {
-#  const user = await userService.getUserById(req.params.userId);
+##  const user = await userService.getUserById(req.params.userId);
 ##  if (!user) {
 ###    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
 #  }
