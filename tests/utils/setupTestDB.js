8ee0afe30d8ae15fd5c3a0ec759a555d73efed40
Developer <noob@developer.com>
2022-11-30 12:15:29 +0000

    Modifying

--- tests/utils/setupTestDB.js
+++ tests/utils/setupTestDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 ####const config = require('../../src/config/config');
-####
+#####
 #####const setupTestDB = () => {
 ######  beforeAll(async () => {
 ####    await mongoose.connect(config.mongoose.url, config.mongoose.options);
