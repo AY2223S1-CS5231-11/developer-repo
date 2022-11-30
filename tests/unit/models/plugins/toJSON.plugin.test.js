996fb0ac93dd8428c1244e6cf4143e8b5cf84442
Developer <noob@developer.com>
2022-11-30 09:39:09 +0000

    Modifying

--- tests/unit/models/plugins/toJSON.plugin.test.js
+++ tests/unit/models/plugins/toJSON.plugin.test.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
   it('should replace _id with id', () => {
     const schema = mongoose.Schema();
 #    schema.plugin(toJSON);
-    const Model = connection.model('Model', schema);
+#    const Model = connection.model('Model', schema);
     const doc = new Model();
 ##    expect(doc.toJSON()).not.toHaveProperty('_id');
 #    expect(doc.toJSON()).toHaveProperty('id', doc._id.toString());
