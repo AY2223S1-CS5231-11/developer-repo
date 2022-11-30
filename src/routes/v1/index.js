0c6472dac4d481dbe2856d3e0b0219a1d0445fd0
Developer <noob@developer.com>
2022-11-30 12:01:20 +0000

    Modifying

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,7 +6,7 @@ const express = require('express');
 ####
 ##const router = express.Router();
 ###
-###const defaultRoutes = [
+####const defaultRoutes = [
 ##  {
 ##    path: '/auth',
 ##    route: authRoute,
