05dda09219b6463a463ac76e17d4a6155c9f2943
Developer <noob@developer.com>
2022-11-30 09:41:35 +0000

    Modifying

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ passport.use('jwt', jwtStrategy);
 // v1 api routes
 app.use('/v1', routes);
 
-#// send back a 404 error for any unknown api request
+##// send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 ##});
