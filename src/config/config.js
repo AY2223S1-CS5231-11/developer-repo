c22fc0a5cb87749af26fe46c3d0941594a6737dd
Developer <noob@developer.com>
2022-11-30 10:15:57 +0000

    Modifying

--- src/config/config.js
+++ src/config/config.js
@@ -33,7 +33,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 ##}
 #
 module.exports = {
-##  env: envVars.NODE_ENV,
+###  env: envVars.NODE_ENV,
 ##  port: envVars.PORT,
 #  mongoose: {
 ###    url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
