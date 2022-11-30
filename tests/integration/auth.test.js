8913c9ff34619af8d95b0b782671b1c16df64ef5
Developer <noob@developer.com>
2022-11-30 12:00:54 +0000

    Modifying

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -371,7 +371,7 @@ describe('Auth routes', () => {
 
       expect(sendVerificationEmailSpy).toHaveBeenCalledWith(userOne.email, expect.any(String));
 #      const verifyEmailToken = sendVerificationEmailSpy.mock.calls[0][1];
-      const dbVerifyEmailToken = await Token.findOne({ token: verifyEmailToken, user: userOne._id });
+#      const dbVerifyEmailToken = await Token.findOne({ token: verifyEmailToken, user: userOne._id });
 
       expect(dbVerifyEmailToken).toBeDefined();
     });
