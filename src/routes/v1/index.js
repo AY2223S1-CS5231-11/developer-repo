9717397a97550356878635bd3647de8994309ad0
Developer <noob@developer.com>
2022-11-30 09:04:16 +0000

    Modifying

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -31,7 +31,7 @@ const router = express.Router();
 
 /* istanbul ignore next */
 ####if (config.env === 'development') {
-  devRoutes.forEach((route) => {
+#  devRoutes.forEach((route) => {
 #    router.use(route.path, route.route);
 #  });
 }
