fc471b9430cdac695e2371a106001cad68be2d29
Developer <noob@developer.com>
2022-11-30 11:55:02 +0000

    Modifying

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -9,7 +9,7 @@ const httpStatus = require('http-status');
 ##  if (!(error instanceof ApiError)) {
 ####    const statusCode =
 ###      error.statusCode || error instanceof mongoose.Error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
-##    const message = error.message || httpStatus[statusCode];
+###    const message = error.message || httpStatus[statusCode];
 ##    error = new ApiError(statusCode, message, false, err.stack);
 ##  }
 #  next(error);
