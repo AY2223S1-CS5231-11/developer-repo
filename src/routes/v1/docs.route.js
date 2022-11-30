3f5706313372d968b524cbf92d5ba6f6a49f7265
Developer <noob@developer.com>
2022-11-30 08:48:02 +0000

    Modifying

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 #const swaggerJsdoc = require('swagger-jsdoc');
-#####const swaggerUi = require('swagger-ui-express');
+######const swaggerUi = require('swagger-ui-express');
 #const swaggerDefinition = require('../../docs/swaggerDef');
 #
 #const router = express.Router();
