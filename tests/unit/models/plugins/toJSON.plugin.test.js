a31bf0cb949fb982ea043a9980ad21abcce24e0e
Developer <noob@developer.com>
2022-11-30 08:36:26 +0000

    Modifying

--- tests/unit/models/plugins/toJSON.plugin.test.js
+++ tests/unit/models/plugins/toJSON.plugin.test.js
@@ -66,7 +66,7 @@ describe('toJSON plugin', () => {
 #  });
 
 #  it('should also call the schema toJSON transform function', () => {
-#    const schema = mongoose.Schema(
+##    const schema = mongoose.Schema(
       {
 #        public: { type: String },
 #        private: { type: String },
