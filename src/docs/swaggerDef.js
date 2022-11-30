0c47650cc121b579a902bdcfb4293364bde6dc8c
Developer <noob@developer.com>
2022-11-30 10:12:36 +0000

    Modifying

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -2,7 +2,7 @@ const { version } = require('../../package.json');
 ####const config = require('../config/config');
 ######
 ####const swaggerDef = {
-###  openapi: '3.0.0',
+####  openapi: '3.0.0',
 ###  info: {
 #####    title: 'node-express-boilerplate API documentation',
 #######    version,
