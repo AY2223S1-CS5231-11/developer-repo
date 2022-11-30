40caaa5322133eb281673425a5716662789638b3
Developer <noob@developer.com>
2022-11-30 09:07:21 +0000

    Modifying

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 #    user: {
 #      type: mongoose.SchemaTypes.ObjectId,
 ###      ref: 'User',
-#      required: true,
+##      required: true,
     },
 #    type: {
 #      type: String,
