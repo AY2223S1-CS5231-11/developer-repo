819c0eb73a3ab937de187f2002df1a76d0c7b666
Developer <noob@developer.com>
2022-11-30 10:05:23 +0000

    Modifying

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -16,7 +16,7 @@ setupTestDB();
     beforeEach(() => {
       newUser = {
         name: faker.name.findName(),
-#        email: faker.internet.email().toLowerCase(),
+##        email: faker.internet.email().toLowerCase(),
         password: 'password1',
         role: 'user',
       };
