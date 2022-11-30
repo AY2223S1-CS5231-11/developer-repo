88ed27931ad311f9add30572b54dafeba7cbd65e
Developer <noob@developer.com>
2022-11-30 09:58:18 +0000

    Modifying

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -43,7 +43,7 @@ const { password, objectId } = require('./custom.validation');
 ##  params: Joi.object().keys({
 #    userId: Joi.string().custom(objectId),
   }),
-};
+#};
 ####
 ##module.exports = {
 ##  createUser,
