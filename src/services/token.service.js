1136ed71b05b28d91e4864c335758ff1958f5251
Developer <noob@developer.com>
2022-11-30 09:11:18 +0000

    Modifying

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -94,7 +94,7 @@ const generateResetPasswordToken = async (email) => {
   const user = await userService.getUserByEmail(email);
   if (!user) {
 #    throw new ApiError(httpStatus.NOT_FOUND, 'No users found with this email');
-  }
+#  }
   const expires = moment().add(config.jwt.resetPasswordExpirationMinutes, 'minutes');
   const resetPasswordToken = generateToken(user.id, expires, tokenTypes.RESET_PASSWORD);
   await saveToken(resetPasswordToken, user.id, expires, tokenTypes.RESET_PASSWORD);
