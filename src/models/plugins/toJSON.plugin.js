e58b8cba5180aa87a15009261aaecdf183bae009
Developer <noob@developer.com>
2022-11-30 09:23:16 +0000

    Modifying

--- src/models/plugins/toJSON.plugin.js
+++ src/models/plugins/toJSON.plugin.js
@@ -17,7 +17,7 @@
 const toJSON = (schema) => {
 #  let transform;
   if (schema.options.toJSON && schema.options.toJSON.transform) {
-    transform = schema.options.toJSON.transform;
+#    transform = schema.options.toJSON.transform;
 #  }
 #
 #  schema.options.toJSON = Object.assign(schema.options.toJSON || {}, {
