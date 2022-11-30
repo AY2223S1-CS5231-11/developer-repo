2cfb337eb61549c238fb7908416c603c825ca3f7
Developer <noob@developer.com>
2022-11-30 08:17:35 +0000

    Modifying

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 #const { authService, userService, tokenService, emailService } = require('../services');
 ##
-const register = catchAsync(async (req, res) => {
+#const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
 #  const tokens = await tokenService.generateAuthTokens(user);
 #  res.status(httpStatus.CREATED).send({ user, tokens });
