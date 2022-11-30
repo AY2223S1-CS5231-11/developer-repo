68bf87e238fa808637988423451a6e3d68ce76eb
Developer <noob@developer.com>
2022-11-30 09:55:06 +0000

    Modifying

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ const httpStatus = require('http-status');
 ###const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { userService } = require('../services');
-
+#
 #const createUser = catchAsync(async (req, res) => {
 #  const user = await userService.createUser(req.body);
 ##  res.status(httpStatus.CREATED).send(user);
