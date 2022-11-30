98a463b7334f8932a77dce1657cbb431f8223094
Developer <noob@developer.com>
2022-11-30 12:11:21 +0000

    Modifying

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -66,7 +66,7 @@ const saveToken = async (token, userId, expires, type, blacklisted = false) => {
 ### * @returns {Promise<Object>}
 ## */
 const generateAuthTokens = async (user) => {
-#  const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+##  const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
   const accessToken = generateToken(user.id, accessTokenExpires, tokenTypes.ACCESS);
 
   const refreshTokenExpires = moment().add(config.jwt.refreshExpirationDays, 'days');
