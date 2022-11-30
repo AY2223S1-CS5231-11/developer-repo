30ae26b14ebd6eda122498b5ac8dba3589d9fbc3
Developer <noob@developer.com>
2022-11-30 10:55:50 +0000

    Modifying

--- src/utils/pick.js
+++ src/utils/pick.js
@@ -2,7 +2,7 @@
 ###### * Create an object composed of the picked object properties
 ###### * @param {Object} object
 ###### * @param {string[]} keys
-######## * @returns {Object}
+######### * @returns {Object}
 #### */
 ###const pick = (object, keys) => {
 ##  return keys.reduce((obj, key) => {
