b739cc1a031b9926cca5a7fbf77aed6265014e42
Developer <noob@developer.com>
2022-11-30 09:40:12 +0000

    Modifying

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -11,7 +11,7 @@ const { tokenTypes } = require('../config/tokens');
 # * @param {string} password
 # * @returns {Promise<User>}
  */
-const loginUserWithEmailAndPassword = async (email, password) => {
+#const loginUserWithEmailAndPassword = async (email, password) => {
 #  const user = await userService.getUserByEmail(email);
 #  if (!user || !(await user.isPasswordMatch(password))) {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
