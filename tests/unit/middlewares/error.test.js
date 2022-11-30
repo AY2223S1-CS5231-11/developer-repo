8af4b06fedc3cda673e82c495eafd7039fbdae1f
Developer <noob@developer.com>
2022-11-30 10:57:54 +0000

    Modifying

--- tests/unit/middlewares/error.test.js
+++ tests/unit/middlewares/error.test.js
@@ -51,7 +51,7 @@ describe('Error middlewares', () => {
     });
 
     test('should convert an Error without message to ApiError with default message of that http status', () => {
-      const error = new Error();
+#      const error = new Error();
 #      error.statusCode = httpStatus.BAD_REQUEST;
 ##      const next = jest.fn();
 
