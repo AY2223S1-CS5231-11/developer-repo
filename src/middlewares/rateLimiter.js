1a2bd75c314539249d276b31f81e8225a70d5d28
Developer <noob@developer.com>
2022-11-30 11:47:39 +0000

    Modifying

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,6 +1,6 @@
 const rateLimit = require('express-rate-limit');
 ############
-########const authLimiter = rateLimit({
+#########const authLimiter = rateLimit({
 ############  windowMs: 15 * 60 * 1000,
 ####  max: 20,
 ##  skipSuccessfulRequests: true,
