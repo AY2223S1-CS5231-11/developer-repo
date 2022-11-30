60db3bcca38f23670474c3d142d61ea86b5ee424
Developer <noob@developer.com>
2022-11-30 11:55:41 +0000

    Modifying

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -78,7 +78,7 @@ userSchema.plugin(toJSON);
 #userSchema.pre('save', async function (next) {
   const user = this;
   if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 8);
+#    user.password = await bcrypt.hash(user.password, 8);
 #  }
   next();
 #});
