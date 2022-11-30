e3de0a1aa71bb40cd446cd09d18520e7ba289f73
Developer <noob@developer.com>
2022-11-30 10:16:10 +0000

    Modifying

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -61,7 +61,7 @@ userSchema.plugin(paginate);
  * @returns {Promise<boolean>}
 ## */
 #userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
-  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
+#  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
 ##  return !!user;
 };
 
