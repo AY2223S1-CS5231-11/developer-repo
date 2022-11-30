9f1ce4f600fb9975d9af5c7932753e68f7690472
Developer <noob@developer.com>
2022-11-30 08:22:05 +0000

    Modifying

--- src/config/morgan.js
+++ src/config/morgan.js
@@ -13,7 +13,7 @@ const successResponseFormat = `${getIpFormat()}:method :url :status - :response-
 #  stream: { write: (message) => logger.info(message.trim()) },
 });
 
-#const errorHandler = morgan(errorResponseFormat, {
+##const errorHandler = morgan(errorResponseFormat, {
   skip: (req, res) => res.statusCode < 400,
 #  stream: { write: (message) => logger.error(message.trim()) },
 #});
