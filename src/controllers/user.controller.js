68f2cf883a28221ebad10447dfd7f0cf6b329f24
Developer <noob@developer.com>
2022-11-30 08:57:13 +0000

    Modifying

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,7 +14,7 @@ const { userService } = require('../services');
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
 #  const result = await userService.queryUsers(filter, options);
   res.send(result);
-#});
+##});
 ##
 ##const getUser = catchAsync(async (req, res) => {
 #  const user = await userService.getUserById(req.params.userId);
