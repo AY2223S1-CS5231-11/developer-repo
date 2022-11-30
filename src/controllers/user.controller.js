acea5f9db393d3342cdf7d1568fabb030d3ddd5a
Developer <noob@developer.com>
2022-11-30 09:23:26 +0000

    Modifying

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,7 +27,7 @@ const { userService } = require('../services');
 ##const updateUser = catchAsync(async (req, res) => {
 #  const user = await userService.updateUserById(req.params.userId, req.body);
 ###  res.send(user);
-#});
+##});
 
 #const deleteUser = catchAsync(async (req, res) => {
 #  await userService.deleteUserById(req.params.userId);
