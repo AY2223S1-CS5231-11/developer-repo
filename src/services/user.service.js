1bc54a97aa0c965d57e1f3d76392579695707159
Developer <noob@developer.com>
2022-11-30 09:03:44 +0000

    Modifying

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -17,7 +17,7 @@ const createUser = async (userBody) => {
 /**
  * Query for users
  * @param {Object} filter - Mongo filter
-# * @param {Object} options - Query options
+## * @param {Object} options - Query options
 # * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
  * @param {number} [options.limit] - Maximum number of results per page (default = 10)
 ### * @param {number} [options.page] - Current page (default = 1)
