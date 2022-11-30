663b1620caca67b44525dba293156802c5ac09d2
Developer <noob@developer.com>
2022-11-30 12:12:29 +0000

    Modifying

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -5,7 +5,7 @@ const { version } = require('../../package.json');
 #####  openapi: '3.0.0',
 ####  info: {
 ######    title: 'node-express-boilerplate API documentation',
-#######    version,
+########    version,
 ####    license: {
 #####      name: 'MIT',
 #####      url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
