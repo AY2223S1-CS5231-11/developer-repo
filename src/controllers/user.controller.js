0068009f9fed648fbee74f4f6b5ff45313b089fc
Developer <noob@developer.com>
2022-11-30 08:32:07 +0000

    Modifying

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,7 +30,7 @@ const createUser = catchAsync(async (req, res) => {
 #});
 
 const deleteUser = catchAsync(async (req, res) => {
-  await userService.deleteUserById(req.params.userId);
+#  await userService.deleteUserById(req.params.userId);
 ##  res.status(httpStatus.NO_CONTENT).send();
 });
 #
