bd06fe9439a1742a78b8b48ae11727c076d19f22
Developer <noob@developer.com>
2022-11-30 12:00:14 +0000

    Modifying

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -5,7 +5,7 @@ const logger = require('../config/logger');
 #const transport = nodemailer.createTransport(config.email.smtp);
 #/* istanbul ignore next */
 ##if (config.env !== 'test') {
-#  transport
+##  transport
 #    .verify()
 #    .then(() => logger.info('Connected to email server'))
 #    .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
