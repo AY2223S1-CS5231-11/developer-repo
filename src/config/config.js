323c176273432c6627b7abf4636f474f3c00ba7a
Developer <noob@developer.com>
2022-11-30 08:45:13 +0000

    Modifying

--- src/config/config.js
+++ src/config/config.js
@@ -29,7 +29,7 @@ const envVarsSchema = Joi.object()
 #const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 #
 #if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
+#  throw new Error(`Config validation error: ${error.message}`);
 #}
 #
 module.exports = {
