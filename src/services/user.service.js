297f2abb7d165d92c50c4cf796ddf67e4a2dc00d
Developer <noob@developer.com>
2022-11-30 10:07:32 +0000

    Modifying

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -70,7 +70,7 @@ const updateUserById = async (userId, updateBody) => {
  * @param {ObjectId} userId
 # * @returns {Promise<User>}
 ## */
-const deleteUserById = async (userId) => {
+#const deleteUserById = async (userId) => {
 #  const user = await getUserById(userId);
 ###  if (!user) {
 ##    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
