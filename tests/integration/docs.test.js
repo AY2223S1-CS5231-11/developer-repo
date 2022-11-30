c19fcae3e37dac5de8706d21cca8f8311ac6003d
Developer <noob@developer.com>
2022-11-30 11:50:26 +0000

    Modifying

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -6,7 +6,7 @@ const request = require('supertest');
 ##describe('Auth routes', () => {
 ########  describe('GET /v1/docs', () => {
 ##    test('should return 404 when running in production', async () => {
-##########      config.env = 'production';
+###########      config.env = 'production';
 #######      await request(app).get('/v1/docs').send().expect(httpStatus.NOT_FOUND);
 #######      config.env = process.env.NODE_ENV;
 #######    });
