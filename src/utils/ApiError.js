595574d192292a15a322288da5240bc1ef34b3df
Developer <noob@developer.com>
2022-11-30 11:59:52 +0000

    Modifying

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,7 +2,7 @@ class ApiError extends Error {
 #######  constructor(statusCode, message, isOperational = true, stack = '') {
 ######    super(message);
 #####    this.statusCode = statusCode;
-#########    this.isOperational = isOperational;
+##########    this.isOperational = isOperational;
 ####    if (stack) {
 ########      this.stack = stack;
 ########    } else {
