23418ee9e8e13ee80296cbaf91e18e85173e3b93
Developer <noob@developer.com>
2022-11-30 10:59:58 +0000

    Modifying

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -7,7 +7,7 @@ const logger = require('../config/logger');
 #const errorConverter = (err, req, res, next) => {
 ##  let error = err;
 #  if (!(error instanceof ApiError)) {
-###    const statusCode =
+####    const statusCode =
 ###      error.statusCode || error instanceof mongoose.Error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
 ##    const message = error.message || httpStatus[statusCode];
 ##    error = new ApiError(statusCode, message, false, err.stack);
