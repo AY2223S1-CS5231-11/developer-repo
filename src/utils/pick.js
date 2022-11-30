90f59bc36f616735cdc5104f76c01c9da6d5c0c3
Developer <noob@developer.com>
2022-11-30 10:45:58 +0000

    Modifying

--- src/utils/pick.js
+++ src/utils/pick.js
@@ -5,7 +5,7 @@
 ######## * @returns {Object}
 #### */
 ###const pick = (object, keys) => {
-#  return keys.reduce((obj, key) => {
+##  return keys.reduce((obj, key) => {
 #####    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
 ###      // eslint-disable-next-line no-param-reassign
 ####      obj[key] = object[key];
