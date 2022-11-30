3a25a67b98ad8af5ea0c42e387c81e8177268d79
Developer <noob@developer.com>
2022-11-30 11:45:51 +0000

    Modifying

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -174,7 +174,7 @@ setupTestDB();
 #    });
 
     test('should return 403 if a non-admin is trying to access all users', async () => {
-      await insertUsers([userOne, userTwo, admin]);
+#      await insertUsers([userOne, userTwo, admin]);
 #
       await request(app)
         .get('/v1/users')
