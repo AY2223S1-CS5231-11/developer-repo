e9d0161d76bdeb8a413e6dd7cce8602965684f41
Developer <noob@developer.com>
2022-11-30 12:05:58 +0000

    Modifying

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -163,7 +163,7 @@ module.exports = router;
  *             properties:
  *               refreshToken:
  *                 type: string
- *             example:
+# *             example:
 # *               refreshToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWJhYzUzNDk1NGI1NDEzOTgwNmMxMTIiLCJpYXQiOjE1ODkyOTg0ODQsImV4cCI6MTU4OTMwMDI4NH0.m1U63blB0MLej_WfB7yC2FTMnCziif9X8yzwDEfJXAg
 ## *     responses:
  *       "200":
