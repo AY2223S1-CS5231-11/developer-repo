e3237c981e2802c82e338861377a653025be03ed
Developer <noob@developer.com>
2022-11-30 10:18:34 +0000

    Modifying

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -7,7 +7,7 @@ const request = require('supertest');
 #####  describe('GET /v1/docs', () => {
 ##    test('should return 404 when running in production', async () => {
 #######      config.env = 'production';
-#####      await request(app).get('/v1/docs').send().expect(httpStatus.NOT_FOUND);
+######      await request(app).get('/v1/docs').send().expect(httpStatus.NOT_FOUND);
 ######      config.env = process.env.NODE_ENV;
 #######    });
 ######  });
