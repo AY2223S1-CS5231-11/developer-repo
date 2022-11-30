b7aff817418f575f7e347ff4e4cf8eab1f678cd1
Developer <noob@developer.com>
2022-11-30 11:26:03 +0000

    Modifying

--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -58,7 +58,7 @@ const paginate = (schema) => {
 ###      const result = {
 ###        results,
 #        page,
-        limit,
+#        limit,
         totalPages,
 ###        totalResults,
 #      };
