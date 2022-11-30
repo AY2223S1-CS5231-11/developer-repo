b1904b4f1bd79003551fe2035a190c63b812182a
Developer <noob@developer.com>
2022-11-30 11:05:55 +0000

    Modifying

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.use(express.json());
 #passport.use('jwt', jwtStrategy);
 
 ####// limit repeated failed requests to auth endpoints
-####if (config.env === 'production') {
+#####if (config.env === 'production') {
   app.use('/v1/auth', authLimiter);
 #}
 #
