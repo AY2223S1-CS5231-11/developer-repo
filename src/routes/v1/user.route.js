a9d7da18ca70c067544ed608732df1a901440f56
Developer <noob@developer.com>
2022-11-30 08:56:28 +0000

    Modifying

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -151,7 +151,7 @@ module.exports = router;
  * /users/{id}:
  *   get:
  *     summary: Get a user
- *     description: Logged in users can fetch only their own user information. Only admins can fetch other users.
+# *     description: Logged in users can fetch only their own user information. Only admins can fetch other users.
 ## *     tags: [Users]
  *     security:
  *       - bearerAuth: []
