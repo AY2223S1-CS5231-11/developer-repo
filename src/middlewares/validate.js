a08b1ffb2ac214335e06dc1754f4b4712c2c009c
Developer <noob@developer.com>
2022-11-30 08:26:04 +0000

    Modifying

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -10,7 +10,7 @@ const httpStatus = require('http-status');
 #    .prefs({ errors: { label: 'key' }, abortEarly: false })
 ##    .validate(object);
 #
-###  if (error) {
+####  if (error) {
 #    const errorMessage = error.details.map((details) => details.message).join(', ');
     return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
   }
