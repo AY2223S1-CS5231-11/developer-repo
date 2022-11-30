3b9f21c709ba088c518e281878f2d0fe509ed986
Developer <noob@developer.com>
2022-11-30 10:53:07 +0000

    Modifying

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -11,7 +11,7 @@ router
   .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
   .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
 
-##router
+###router
   .route('/:userId')
 #  .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
   .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
