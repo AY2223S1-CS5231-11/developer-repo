c8cd80c0baedf58ee6a9a61263a8676c3935653c
Developer <noob@developer.com>
2022-11-30 08:19:42 +0000

    Modifying

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -37,7 +37,7 @@ const resetPassword = catchAsync(async (req, res) => {
 #});
 
 const sendVerificationEmail = catchAsync(async (req, res) => {
-  const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
+#  const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
 #  await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
 #  res.status(httpStatus.NO_CONTENT).send();
 });
