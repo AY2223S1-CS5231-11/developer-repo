13c337dad47ce92f5749b9957c06a8e7a763ce12
Developer <noob@developer.com>
2022-11-30 09:41:33 +0000

    Modifying

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -15,7 +15,7 @@ const logger = require('../config/logger');
 #  next(error);
 #};
 
-// eslint-disable-next-line no-unused-vars
+#// eslint-disable-next-line no-unused-vars
 ##const errorHandler = (err, req, res, next) => {
   let { statusCode, message } = err;
 #  if (config.env === 'production' && !err.isOperational) {
