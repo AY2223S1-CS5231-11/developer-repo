c085cfdb49bc27c760470a06addb5c5c2ba21662
Developer <noob@developer.com>
2022-11-30 08:32:13 +0000

    Modifying

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,7 +3,7 @@ const authRoute = require('./auth.route');
 ##const userRoute = require('./user.route');
 const docsRoute = require('./docs.route');
 ###const config = require('../../config/config');
-
+#
 const router = express.Router();
 #
 #const defaultRoutes = [
