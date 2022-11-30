e51aa6800624712d902842c0042ffe04a062ffc2
Developer <noob@developer.com>
2022-11-30 11:27:14 +0000

    Modifying

--- src/config/roles.js
+++ src/config/roles.js
@@ -2,7 +2,7 @@ const allRoles = {
 ######  user: [],
 ######  admin: ['getUsers', 'manageUsers'],
 #######};
-########
+#########
 ##const roles = Object.keys(allRoles);
 #######const roleRights = new Map(Object.entries(allRoles));
 #########
