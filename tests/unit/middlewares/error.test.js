a4301915d2388d4fcfe8a3b0c004fd4c8eaf6c91
Developer <noob@developer.com>
2022-11-30 12:10:16 +0000

    Modifying

--- tests/unit/middlewares/error.test.js
+++ tests/unit/middlewares/error.test.js
@@ -24,7 +24,7 @@ describe('Error middlewares', () => {
 
 #      errorConverter(error, httpMocks.createRequest(), httpMocks.createResponse(), next);
 
-      expect(next).toHaveBeenCalledWith(expect.any(ApiError));
+#      expect(next).toHaveBeenCalledWith(expect.any(ApiError));
       expect(next).toHaveBeenCalledWith(
 ###        expect.objectContaining({
 #          statusCode: error.statusCode,
