57bd112d0d3f5163105b2a822946adbd2ddf9a26
Developer <noob@developer.com>
2022-11-30 10:45:56 +0000

    Modifying

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,7 +6,7 @@ const Token = require('../models/token.model');
 const { tokenTypes } = require('../config/tokens');
 
 /**
- * Login with username and password
+# * Login with username and password
  * @param {string} email
 # * @param {string} password
 ## * @returns {Promise<User>}
