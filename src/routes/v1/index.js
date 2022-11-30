22c7a552d0275e58ea91a7e66c558a0aa3f5feaf
Developer <noob@developer.com>
2022-11-30 12:11:30 +0000

    Modifying

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -32,7 +32,7 @@ const express = require('express');
 ##/* istanbul ignore next */
 #####if (config.env === 'development') {
 #  devRoutes.forEach((route) => {
-##    router.use(route.path, route.route);
+###    router.use(route.path, route.route);
 #  });
 ###}
 ######
