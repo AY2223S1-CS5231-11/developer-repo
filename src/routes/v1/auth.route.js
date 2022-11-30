499446d9d8d1acfb1d7bb25ce1dcd0d57702f22c
Developer <noob@developer.com>
2022-11-30 09:19:37 +0000

    Modifying

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -165,7 +165,7 @@ module.exports = router;
  *                 type: string
  *             example:
  *               refreshToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWJhYzUzNDk1NGI1NDEzOTgwNmMxMTIiLCJpYXQiOjE1ODkyOTg0ODQsImV4cCI6MTU4OTMwMDI4NH0.m1U63blB0MLej_WfB7yC2FTMnCziif9X8yzwDEfJXAg
-# *     responses:
+## *     responses:
  *       "200":
  *         description: OK
  *         content:
