d4c8739f0342f6ccee7efd0f031e1ea82d03adff
Developer <noob@developer.com>
2022-11-30 11:58:11 +0000

    Modifying

--- src/validations/custom.validation.js
+++ src/validations/custom.validation.js
@@ -11,7 +11,7 @@ const objectId = (value, helpers) => {
   }
 ######  if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
 ###    return helpers.message('password must contain at least 1 letter and 1 number');
-########  }
+#########  }
 ###  return value;
 #####};
 ######
