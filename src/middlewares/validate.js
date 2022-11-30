bb5f8d352720fbd403d852de9d7ecf8d35f170c7
Developer <noob@developer.com>
2022-11-30 11:30:19 +0000

    Modifying

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 #####const pick = require('../utils/pick');
 ######const ApiError = require('../utils/ApiError');
 #######
-####const validate = (schema) => (req, res, next) => {
+#####const validate = (schema) => (req, res, next) => {
 ###  const validSchema = pick(schema, ['params', 'query', 'body']);
 ######  const object = pick(req, Object.keys(validSchema));
 #####  const { value, error } = Joi.compile(validSchema)
