eca24971ea9936fa4983a5355682faf474f692a4
Developer <noob@developer.com>
2022-11-30 11:16:03 +0000

    Modifying

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -45,7 +45,7 @@ const logger = require('../config/logger');
 # * @param {string} token
 # * @returns {Promise}
 # */
-const sendVerificationEmail = async (to, token) => {
+#const sendVerificationEmail = async (to, token) => {
 ##  const subject = 'Email Verification';
 #  // replace this url with the link to the email verification page of your front-end app
 ##  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
