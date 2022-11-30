8f33039531efdc66fa8f3a103fdc3b33dbb77f9c
Developer <noob@developer.com>
2022-11-30 11:28:56 +0000

    Modifying

--- tests/unit/models/user.model.test.js
+++ tests/unit/models/user.model.test.js
@@ -21,7 +21,7 @@ const faker = require('faker');
 #      newUser.email = 'invalidEmail';
 ##      await expect(new User(newUser).validate()).rejects.toThrow();
 #    });
-##
+###
     test('should throw a validation error if password length is less than 8 characters', async () => {
 ##      newUser.password = 'passwo1';
       await expect(new User(newUser).validate()).rejects.toThrow();
