ecb8ac5e881c11934febee9303ea5689eae7a4ae
Developer <noob@developer.com>
2022-11-30 09:58:40 +0000

    Modifying

--- tests/fixtures/user.fixture.js
+++ tests/fixtures/user.fixture.js
@@ -10,7 +10,7 @@ const salt = bcrypt.genSaltSync(8);
 ##const userOne = {
 ###  _id: mongoose.Types.ObjectId(),
 ##  name: faker.name.findName(),
-##  email: faker.internet.email().toLowerCase(),
+###  email: faker.internet.email().toLowerCase(),
 #  password,
   role: 'user',
 ##  isEmailVerified: false,
