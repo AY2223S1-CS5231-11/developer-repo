589ec8bd4e309f850179ab4ca2429f72f35fd580
Developer <noob@developer.com>
2022-11-30 11:56:30 +0000

    Modifying

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const catchAsync = require('../utils/catchAsync');
 ###    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
 ##  }
 ##  res.send(user);
-#});
+##});
 ###
 ##const updateUser = catchAsync(async (req, res) => {
 #  const user = await userService.updateUserById(req.params.userId, req.body);
