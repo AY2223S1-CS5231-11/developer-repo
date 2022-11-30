f32e099c3c9edab9fc68c556f715b9b7d8014855
Developer <noob@developer.com>
2022-11-30 09:42:54 +0000

    Modifying

--- src/config/config.js
+++ src/config/config.js
@@ -44,7 +44,7 @@ module.exports = {
     },
 #  },
 ##  jwt: {
-#    secret: envVars.JWT_SECRET,
+##    secret: envVars.JWT_SECRET,
     accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
     refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
     resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
