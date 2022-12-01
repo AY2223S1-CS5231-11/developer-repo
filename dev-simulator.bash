3e84da1f51911f9e0668a4c86f2c93f0bf1908c1
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-12-01 09:31:46 +0800

    Update dev-simulator.bash

--- dev-simulator.bash
+++ dev-simulator.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
-####set -x
-##
-#MAXWAIT=10  # seconds
-#####
-##while true; do 
-######	PATH_TO_NEXT_FILE_TO_MODIFY=$(find . -type f -not -path "*git/*" -not -path "dev-simulator.bash" | shuf -n 1)
-###	FILENAME=${PATH_TO_NEXT_FILE_TO_MODIFY##*/}
-#	WAIT_DURATION=$((RANDOM % MAXWAIT))
-#####
-#####	echo "Modifying $FILENAME at $PATH_TO_NEXT_FILE_TO_MODIFY"
-###	echo "Devving for $WAIT_DURATION seconds.."
-######	sleep $WAIT_DURATION
-###
-###	FILE_NUMBER_OF_LINES=$(wc -l < $PATH_TO_NEXT_FILE_TO_MODIFY)
-#	LINE_NUMBER_TO_TWEAK=$(shuf -i 2-$FILE_NUMBER_OF_LINES -n 1)
-###	echo "Tweaking line number $LINE_NUMBER_TO_TWEAK"
-##	sed -i "$LINE_NUMBER_TO_TWEAK"'s/^/#/' "$PATH_TO_NEXT_FILE_TO_MODIFY"
-####
-###	git add -u
-#	git commit -m "Modifying $FILENAME"
-###	git push
-#####done 
+set -x
+
+MAXWAIT=10  # seconds
+
+while true; do 
+	PATH_TO_NEXT_FILE_TO_MODIFY=$(find . -type f -not -path "*git/*" -not -path "dev-simulator.bash" | shuf -n 1)
+	FILENAME=${PATH_TO_NEXT_FILE_TO_MODIFY##*/}
+	WAIT_DURATION=$((RANDOM % MAXWAIT))
+
+	echo "Modifying $FILENAME at $PATH_TO_NEXT_FILE_TO_MODIFY"
+	echo "Devving for $WAIT_DURATION seconds.."
+	sleep $WAIT_DURATION
+
+	FILE_NUMBER_OF_LINES=$(wc -l < $PATH_TO_NEXT_FILE_TO_MODIFY)
+	LINE_NUMBER_TO_TWEAK=$(shuf -i 2-$FILE_NUMBER_OF_LINES -n 1)
+	echo "Tweaking line number $LINE_NUMBER_TO_TWEAK"
+	sed -i "$LINE_NUMBER_TO_TWEAK"'s/^/#/' "$PATH_TO_NEXT_FILE_TO_MODIFY"
+  
+	git add -u
+	git commit -m "Modifying $FILENAME"
+	git push
+done 
